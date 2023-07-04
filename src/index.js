164986c14824bbec4c9e92384c387c0c12611b36
Mazin Islam <93228608+N1ghtHunter@users.noreply.github.com>
2023-07-04 21:09:51 +0300

    Load dynamic projects from a backend server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,72 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
+
+const projectContainer = document.querySelector("#projects-container");
+
+(async () => {
+  try {
+    const getProjects = async () => {
+      const response = await fetch("http://localhost:3008/api/projects");
+      const data = await response.json();
+      return data;
+    }
+
+    const projects = await getProjects();
+    const createProject = (project) => {
+      const projectElement = document.createElement("div");
+      projectElement.classList.add("row");
+
+      const projectText = document.createElement("div");
+      projectText.classList.add("col-lg-4", "col-sm-12");
+      projectText.innerHTML = `
+               <div class="project-wrapper__text load-hidden">
+               <h3 class="project-wrapper__text-title">${project.title}</h3>
+               <div>
+               <p class="mb-4">
+               ${project.description}
+               </p>
+               </div>
+               <a rel="noreferrer" target="_blank" class="cta-btn cta-btn--hero" href="${project.live_demo}">
+               See Live
+               </a>
+               <a rel="noreferrer" target="_blank" class="cta-btn text-color-main" href="${project.source_code}">
+               Source Code
+               </a>
+               </div>
+               `;
+
+      const projectImage = document.createElement("div");
+      projectImage.classList.add("col-lg-8", "col-sm-12");
+      projectImage.innerHTML = `
+               <div class="project-wrapper__image load-hidden">
+               <a rel="noreferrer" href="${project.link}" target="_blank">
+               <div data-tilt data-tilt-max="4" data-tilt-glare="true" data-tilt-max-glare="0.5" class="thumbnail rounded js-tilt">
+               <img alt="Project Image" class="img-fluid" src="http://localhost:3008${project.image_path}"
+                style=" aspect-ratio: 16/9;"
+               />
+               </div>
+               </a>
+               </div>
+               `;
+
+      projectElement.appendChild(projectText);
+      projectElement.appendChild(projectImage);
+
+      return projectElement;
+    }
+
+    projects.forEach(project => {
+      projectContainer.appendChild(createProject(project));
+    }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  initScrollReveal(targetElements, defaultProps);
+  initTiltEffect();
+})();
+
+
+
